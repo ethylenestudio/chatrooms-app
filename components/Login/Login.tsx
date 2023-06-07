1113bb394bce956da74d1a915c8d551171a53260
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-06-07 14:46:22 +0300

    feat: room store to fetch user allowed contexts

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,74 +1,38 @@
 "use client";
 // @ts-expect-error
 import { Orbis } from "@orbisclub/orbis-sdk";
-
-import useUser from "@/hooks/useUser";
-import React, { FC, useEffect } from "react";
-
+import React, { FC, useEffect, useMemo } from "react";
+import useRooms from "@/hooks/useRooms";
 const Login: FC = () => {
-  const setUser = useUser((state) => state.setUser);
-  const user = useUser((state) => state.user);
-  const orbis = useUser((state) => state.orbis);
+  const setRooms = useRooms((state) => state.setRooms);
+
+  const orbis = useMemo(() => new Orbis(), []);
 
   useEffect(() => {
     async function update() {
       let res = await orbis.isConnected();
-      if (!res) return;
-      setUser(res.did);
-      console.log("connected");
+      if (res.status == 200) {
+        const { data: creds } = await orbis.getCredentials(res.did);
+        setRooms(creds);
+      }
     }
     update();
-  }, [orbis, user, setUser]);
+  }, [orbis, setRooms]);
 
   async function connect() {
     let res = await orbis.isConnected();
-    if (res) {
-      setUser(res.did);
-    } else {
-      let res = await orbis.connect_v2({ lit: false, chain: "ethereum" });
-      setUser(res.did);
+    if (res.status != 200) {
+      res = await orbis.connect_v2({ lit: false, chain: "ethereum" });
     }
-    console.log(orbis);
-  }
-  async function createContext() {
-    let res = await orbis.createContext({
-      project_id: "kjzl6cwe1jw148cji7hgrna5ta4w5wyo0fpovye9m80xod0vtdbe9rbrb4xfbn8",
-      name: "Chatrooms-EF",
-      websiteUrl: "https://ethylene.io",
-    });
-    console.log(res, "take this");
-  }
-
-  async function createProject() {
-    let res = await orbis.createProject({
-      name: "Chatrooms-EF",
-    });
-    console.log(res);
-  }
-
-  async function getContexts() {
-    let { data, error } = await orbis.getContexts(
-      "kjzl6cwe1jw148cji7hgrna5ta4w5wyo0fpovye9m80xod0vtdbe9rbrb4xfbn8"
-    );
-    console.log(data);
-  }
-
-  async function connectAsAdmin() {
-    const res = await orbis.connectWithSeed([
-      205, 228, 76, 81, 250, 24, 149, 235, 47, 72, 245, 54, 151, 217, 79, 178, 76, 66, 235, 169,
-      199, 166, 71, 232, 60, 246, 192, 176, 202, 106, 224, 63,
-    ]);
-    console.log(res);
+    const { data: creds } = await orbis.getCredentials(res.did);
+    setRooms(creds);
   }
 
   return (
-    <div>
-      <button onClick={() => console.log(user)}>click me </button>
-      <button onClick={connect}>Connect </button>
-      <button onClick={createContext}>contextcreate </button>
-      <button onClick={createProject}>projectttt </button>
-      <button onClick={getContexts}>CONTEXTS </button>
-      <button onClick={connectAsAdmin}>ADMIN </button>
+    <div className="text-white flex justify-center items-center h-[80vh]">
+      <button className="rounded-md border-2 border-white py-3 px-4" onClick={connect}>
+        Connect
+      </button>
     </div>
   );
 };
---
+++ hooks/useRooms.ts
@@ -0,0 +1,60 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+const useRooms = create(
+  persist(
+    devtools<StoreType>((set) => ({
+      rooms: [],
+      setRooms: (rooms) => set({ rooms }, false, "setRooms"),
+      pushRoom: (room) => set((prev) => ({ rooms: [...prev.rooms, room] }), false, "pushRoom"),
+      clearRooms: () => set({ rooms: [] }, false, "clearRooms"),
+    })),
+    {
+      name: "roomData",
+      // merge: (persistedState: any, currentState) => {
+      //   console.log(persistedState, "persisted state");
+      //   return persistedState["rooms"];
+      // },
+    }
+  )
+);
+
+type RoomType = {
+  stream_id: string;
+  issuer: string;
+  subject_id: string;
+  content: {
+    issuer: {
+      id: string;
+      name: string;
+    };
+    "@context": string[];
+    issuanceDate: string;
+    credentialSubject: {
+      id: string;
+      name: string;
+      type: string;
+      network: "EVM";
+      protocol: string;
+      description: string;
+    };
+  };
+  provider: null;
+  created_at: string;
+  timestamp: number;
+  creator: string;
+  family: string;
+  hash: null;
+  weight: number;
+  identifier: `eth-barcelona-chatrooms-access-${string}`;
+};
+type SetRoomsType = (rooms: RoomType[]) => void;
+type PushRoomType = (room: RoomType) => void;
+type ClearRoomsType = () => void;
+type StoreType = {
+  rooms: RoomType[];
+  setRooms: SetRoomsType;
+  pushRoom: PushRoomType;
+  clearRooms: ClearRoomsType;
+};
+
+export default useRooms;
