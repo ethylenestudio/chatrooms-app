1a96cba225969ab6462038dd810d1876d7b53ce8
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-06-09 17:33:15 +0300

    feat: new projId for beta

--- config.ts
+++ config.ts
@@ -3,9 +3,9 @@ import { Orbis } from "@orbisclub/orbis-sdk";
 export const lastMessageLimit = 34;
 export const replyLimit = 30;
 export const APIROOT = "http://localhost:8000";
-export const ORBIS_IDENTIFIER = "eth-barcelona-chatrooms";
+export const ORBIS_IDENTIFIER = "eth-barcelona-chatrooms-beta";
 export const ORBIS = new Orbis();
 export const GENERAL_CHAT = "kjzl6cwe1jw149kggo4zzbrbdp53jnp4u0cw7x6xvpq31wzfjxuvq04asrk9qsm";
 export const POLLING_RATE = 5000; //ms
 export const MESSAGE = "Signing to access chatrooms admin panel";
-export const ORBIS_PROJECT_ID = "kjzl6cwe1jw145b59614y5hw24bicmwcift03o3xn08w1lqnklzqvhplpz66grz";
+export const ORBIS_PROJECT_ID = "kjzl6cwe1jw145nessm47mky8qclcfi0eh4y07892xqy5e1leeoujfd301m259x";
