60ffce6df3fe3de38bd1b37474da0a288130bc49
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-06-15 12:03:49 +0300

    refactor: checkConnection added to its own hook

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,24 +1,14 @@
 "use client";
 import { Chat, Menu } from "@/components";
-import { ORBIS } from "@/config";
 import useSelectRoom from "@/hooks/store/useSelectRoom";
 import useWindowSize from "@/hooks/useWindowSize";
-import { useRouter } from "next/navigation";
-import React, { useCallback, useEffect } from "react";
+import React from "react";
+import useCheckConnection from "@/hooks/useCheckConnection";
 
 const App = () => {
-  const router = useRouter();
-  const checkConnection = useCallback(async () => {
-    let res = await ORBIS.isConnected();
-    if (res.status != 200) {
-      router.push("/");
-    }
-  }, [router]);
+  useCheckConnection();
   const [width, height] = useWindowSize();
   const selectedChat = useSelectRoom((state) => state.selectedRoom);
-  useEffect(() => {
-    checkConnection();
-  }, [checkConnection]);
 
   if (width <= 760) {
     return (
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,24 +1,11 @@
 "use client";
 import { Chat } from "@/components";
-import { ORBIS } from "@/config";
 import useSelectRoom from "@/hooks/store/useSelectRoom";
-import { useRouter } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import useCheckConnection from "@/hooks/useCheckConnection";
 
 export default function ChatPage() {
   const selectedChat = useSelectRoom((state) => state.selectedRoom);
-  const router = useRouter();
-
-  const checkConnection = useCallback(async () => {
-    let res = await ORBIS.isConnected();
-    if (res.status != 200) {
-      router.push("/");
-    }
-  }, [router]);
-
-  useEffect(() => {
-    checkConnection();
-  }, [checkConnection]);
+  useCheckConnection();
 
   return (
     <div className="overflow-y-auto w-[100%]">
