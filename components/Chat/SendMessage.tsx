da93352fd752748fc04b05438515b22b618d31d6
Donat Andelic <34493754+donatandelic@users.noreply.github.com>
2023-06-15 18:58:51 +0200

    Input adjustments

--- components/Chat/SendMessage.tsx
+++ components/Chat/SendMessage.tsx
@@ -1,6 +1,6 @@
 import { renderMessageLimit, replyLimit } from "@/config";
 import { usePathname } from "next/navigation";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import Loader from "../ui/Loader";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
@@ -22,29 +22,32 @@ const SendMessage: FC<{
   message: string;
 }> = ({ replyTo, setReplyTo, message, setMessage, sendMessage, sending }) => {
   const pathname = usePathname();
+  const [focused, setFocused] = useState<boolean>(false)
 
   return (
     <div
-      className={`h-[75px] sticky bottom-0 left-0 flex flex-col space-y-2 justify-center bg-[rgba(8,9,13,1)] w-full`}
+      className={`sticky bottom-0 left-0 flex flex-col p-3 justify-center bg-[rgba(8,9,13,1)] w-full`}
     >
-      <div className={`text-white flex justify-end items-center pl-2 text-xs px-4`}>
-        <p>Chars: {renderMessageLimit - message.length}</p>
-      </div>
-      <form className="flex justify-center space-x-2 px-4 w-full items-center" action="submit">
+      <form className={`w-full items-center flex outline-1 pr-1 border-[1px] ${focused && "border-white" || "border-slate-400"} rounded-[100px]`} action="submit">
         <input
-          placeholder="Ask a question"
-          className="outline-1 border-[1px] border-slate-400 text-slate-400 outline-black rounded-2xl text-sm px-4 py-1 w-[90%] bg-[rgba(0,0,0,0.2)]"
-          type="text"
-          value={message}
-          onChange={(e) =>
-            setMessage((prev) => {
-              if (prev.length < renderMessageLimit || e.target.value.length <= renderMessageLimit) {
-                return e.target.value;
-              }
-              return prev;
-            })
-          }
-        />
+            placeholder="Ask a question"
+            className="flex-1 rounded-2xl text-white py-2 mr-2 outline-black outline-0 text-sm px-4 bg-[rgba(0,0,0,0.2)]"
+            type="text"
+            value={message}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            onChange={(e) =>
+              setMessage((prev) => {
+                if (prev.length < renderMessageLimit || e.target.value.length <= renderMessageLimit) {
+                  return e.target.value;
+                }
+                return prev;
+              })
+            }
+          />
+        <div className="text-white text-xs mr-2">
+          <span className={message.length > renderMessageLimit && "text-rose-600 font-semibold" || ""}>{message.length}</span> / {renderMessageLimit}
+        </div>
         <button
           type="submit"
           onClick={async (e) => {
