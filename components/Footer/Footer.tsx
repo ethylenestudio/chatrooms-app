fd326af3dd7035196a9209a41345c3ac1866d7d2
aalimsahin <alimsahin0007@gmail.com>
2023-06-19 15:05:49 +0300

    fix: added fixed position to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,8 +2,10 @@ import React, { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <div className="pb-[env(safe-area-inset-bottom)] w-full bg-[rgba(0,0,0,1)] text-white flex items-center justify-center col-span-2 row-start-3">
-      <p className="text-sm font-extralight tracking-wide">Powered by Ethylene - Orbis - DoinGud</p>
+    <div className="pb-[env(safe-area-inset-bottom)] w-full bg-[rgba(0,0,0,1)] text-white flex items-center justify-center col-span-2 row-start-3 fixed bottom-0 h-12">
+      <p className="text-sm font-extralight tracking-wide">
+        Powered by Ethylene - Orbis - DoinGud
+      </p>
     </div>
   );
 };
