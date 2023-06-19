ede66e90c154e3bbc5f61c7d8a3632e26085826a
aalimsahin <alimsahin0007@gmail.com>
2023-06-19 15:35:23 +0300

    fix: updated footer height

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <div className="pb-[env(safe-area-inset-bottom)] w-full bg-[rgba(0,0,0,1)] text-white flex items-center justify-center col-span-2 row-start-3 fixed bottom-0 h-12">
+    <div className="pb-[env(safe-area-inset-bottom)] w-full bg-[rgba(0,0,0,1)] text-white flex items-center justify-center col-span-2 row-start-3 fixed bottom-0 h-14">
       <p className="text-sm font-extralight tracking-wide">
         Powered by Ethylene - Orbis - DoinGud
       </p>
