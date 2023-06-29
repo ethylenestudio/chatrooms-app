35e73efa4bc2bd779024f1263fca75931c86a97a
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-06-29 17:56:23 +0300

    fix: wconnect v2 impl

--- hooks/useRainbow.tsx
+++ hooks/useRainbow.tsx
@@ -1,43 +1,30 @@
 "use client";
 import "@rainbow-me/rainbowkit/styles.css";
-import {
-  injectedWallet,
-  metaMaskWallet,
-  walletConnectWallet,
-} from "@rainbow-me/rainbowkit/wallets";
 import merge from "lodash.merge";
-import { connectorsForWallets, darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { PropsWithChildren } from "react";
 import { WC_PROJECT_ID } from "@/config";
 
-const { chains, provider } = configureChains([mainnet], [publicProvider()]);
+const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
 
-const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended",
-    wallets: [
-      metaMaskWallet({ chains, projectId: WC_PROJECT_ID }),
-      walletConnectWallet({
-        chains,
-        projectId: WC_PROJECT_ID,
-        options: { projectId: WC_PROJECT_ID },
-      }),
-    ],
-  },
-]);
+const { connectors } = getDefaultWallets({
+  appName: "Chatrooms",
+  projectId: WC_PROJECT_ID,
+  chains,
+});
 
-const wagmiClient = createClient({
+const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
-  provider,
+  publicClient,
 });
 
 export const RainbowProvider = ({ children }: PropsWithChildren) => {
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider
         theme={merge(
           darkTheme({
