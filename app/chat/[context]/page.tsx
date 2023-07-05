456ef93ef7f2523937f178772c8bd4343629b623
Donat Andelic <34493754+donatandelic@users.noreply.github.com>
2023-07-05 06:46:48 +0200

    Show session title in presenter mode

--- app/chat/[context]/page.tsx
+++ app/chat/[context]/page.tsx
@@ -8,6 +8,7 @@ import { useRouter, useParams } from "next/navigation";
 export default function ChatPage() {
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(true)
+  const [title, setTitle] = useState<string>("")
 
   useEffect(() => {
     (async () => {
@@ -16,14 +17,26 @@ export default function ChatPage() {
     })()
   }, [])
 
-
   const { context } = useParams()
   if (!context) {
     router.push("/")
     return <h1>Redirecting...</h1>
   }
 
-  if(loading) return <h1>Loading chat...</h1>
+  useEffect(() => {
+    (async () => {
+      const { data: info } = await ORBIS.getContext(context)
+      if(info.content?.name) setTitle(info.content.name as string)
+    })()
+  }, [context])
 
-  return <Chat context={context as string} className="col-span-2" />;
+  if (loading) return <h1>Loading chat...</h1>
+
+  return <div className="col-span-2">
+    {title && <h1 className="text-center text-medium text-white pb-4">
+      {title}
+    </h1>}
+    <Chat context={context as string} className="col-span-2 h-full" />
+  </div>
+
 }
