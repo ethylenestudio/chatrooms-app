eafdc996a5ab57525d0de8fb2fdf1f2201276342
Donat Andelic <34493754+donatandelic@users.noreply.github.com>
2023-07-05 11:56:24 +0200

    refresh button attempt

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -6,16 +6,21 @@ import Conversation from "./Conversation";
 import useHydrated from "@/hooks/useHydrated";
 import { ORBIS, POLLING_RATE } from "@/config";
 import useWindowSize from "@/hooks/useWindowSize";
+import useFetchEligibleRooms from "@/hooks/useFetchEligibleRooms";
 
 const Menu: FC = () => {
   const hasHydrated = useHydrated();
   const rooms = useRooms((state) => state.rooms);
+  const setRooms = useRooms(state => state.setRooms)
   const [lastMessages, setLastMessages] = useState<string[]>([]);
+  const [loadingRooms, setLoadingRooms] = useState<boolean>(false)
+  const { updateRoomAccess } = useFetchEligibleRooms()
+
   const fetchLastMessage = async (room: any) => {
     const { data } = await ORBIS.getPosts({ context: room.stream_id, only_master: true }, 0, 1);
     return data;
   };
-  const [width, height] = useWindowSize();
+
   useEffect(() => {
     if (rooms == null) return;
     const polling = async () => {
@@ -37,13 +42,27 @@ const Menu: FC = () => {
       clearInterval(pollingInterval);
     };
   }, [rooms]);
+
+  const updateAccess = async () => {
+    if (loadingRooms) return
+    setLoadingRooms(true)
+    try { await updateRoomAccess(setRooms) } catch { }
+    setLoadingRooms(false)
+  }
+
+
   if (!hasHydrated) return null;
   return (
     <div className="relative">
       <div
         className={`w-full bg-[#090A10] border-r-2 border-slate-900 sticky top-0 left-0`}
       >
-        <p className="text-[#CBA1A4] text-xs pt-2 text-center">Rooms</p>
+        <p className="text-[#CBA1A4] text-xs pt-2 text-center relative">
+          Rooms
+          <span className="absolute cursor-pointer text-white right-[1em] text-[.9em] uppercase" onClick={() => updateAccess()}>
+            {loadingRooms ? "Refreshing..." : "Refresh"}
+          </span>
+        </p>
         <GeneralConversation />
       </div>
       {rooms?.map((room: any, i: number) => {
